fbe93b4bb4b63db41bdd01d2c659a9e352dd7410
John Wass <wassj@ctc.com>
2018-12-02 00:05:59 -0500

    reformat

--- src/arbiter_example.cpp
+++ src/arbiter_example.cpp
@@ -2,38 +2,37 @@
 #include "BuildInfo.hpp"
 
 
-
 namespace pdal
 {
-  static PluginInfo const s_info{
-          BuildInfo::PDALStageName,
-          BuildInfo::PluginDescription,
-          BuildInfo::PluginURL
-  };
+   static PluginInfo const s_info{
+         BuildInfo::PDALStageName,
+         BuildInfo::PluginDescription,
+         BuildInfo::PluginURL
+   };
 
-    CREATE_SHARED_STAGE(arbiter_example, s_info)
+   CREATE_SHARED_STAGE(arbiter_example, s_info)
 
-    std::string arbiter_example::getName() const {
-        return s_info.name;
-    }
+   std::string arbiter_example::getName() const {
+      return s_info.name;
+   }
 
-    void arbiter_example::addArgs(ProgramArgs& args) {
-        std::cout << BuildInfo::PDALStageName << "::addArgs" << std::endl;
-    }
+   void arbiter_example::addArgs(ProgramArgs& args) {
+      std::cout << BuildInfo::PDALStageName << "::addArgs" << std::endl;
+   }
 
-    void arbiter_example::ready(PointTableRef table) {
-        std::cout << BuildInfo::PDALStageName << "::ready" << std::endl;
-    }
+   void arbiter_example::ready(PointTableRef table) {
+      std::cout << BuildInfo::PDALStageName << "::ready" << std::endl;
+   }
 
-    PointViewSet arbiter_example::run(PointViewPtr view) {
-        std::cout << BuildInfo::PDALStageName << "::run" << std::endl;
+   PointViewSet arbiter_example::run(PointViewPtr view) {
+      std::cout << BuildInfo::PDALStageName << "::run" << std::endl;
 
-        PointViewSet viewSet;
-        viewSet.insert(view);
-        return viewSet;
-    }
+      PointViewSet viewSet;
+      viewSet.insert(view);
+      return viewSet;
+   }
 
-    void arbiter_example::done(PointTableRef) {
-        std::cout << BuildInfo::PDALStageName << "::done" << std::endl;
-    }
+   void arbiter_example::done(PointTableRef) {
+      std::cout << BuildInfo::PDALStageName << "::done" << std::endl;
+   }
 }
--- src/arbiter_example.hpp
+++ src/arbiter_example.hpp
@@ -4,15 +4,15 @@
 
 namespace pdal
 {
-    class PDAL_DLL arbiter_example : public Filter
-    {
-    public:
-        std::string getName() const override;
+   class PDAL_DLL arbiter_example : public Filter
+   {
+   public:
+      std::string getName() const override;
 
-    private:
-        void addArgs(ProgramArgs&) override;
-        void ready(PointTableRef) override;
-        PointViewSet run(PointViewPtr) override;
-        void done(PointTableRef) override;
-    };
+   private:
+      void addArgs(ProgramArgs&) override;
+      void ready(PointTableRef) override;
+      PointViewSet run(PointViewPtr) override;
+      void done(PointTableRef) override;
+   };
 }
